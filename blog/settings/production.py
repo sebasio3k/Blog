d179de1d6125df1b1a4d4eec1096f7675b703c99
SebastianHO <bastianhdezo@gmail.com>
2022-08-02 16:26:16 -0500

    env

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -13,10 +13,10 @@ ALLOWED_HOSTS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dc0ckpt5hr14p3',
-        'USER': 'wjoqvvvmpsmhln',
-        'PASSWORD': '9eb0d95099e0981f514a9fa8b8f26a4f384778397f3ffbdee50b1592c813a6c1',
-        'HOST': 'ec2-54-86-224-85.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PWD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
