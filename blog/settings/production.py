f8896e6411042bb45724e55c0d58a764c7df58ff
SebastianHO <bastianhdezo@gmail.com>
2022-08-02 14:52:02 -0500

    allowed host

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -3,7 +3,9 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'django-blog-hodev.herokuapp.com',
+]
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
