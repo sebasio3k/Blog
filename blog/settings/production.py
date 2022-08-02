8b92f24b9773ff2980efbd2403002f49608c4e4a
SebastianHO <bastianhdezo@gmail.com>
2022-08-02 14:49:12 -0500

    bd conf

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -11,10 +11,10 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blog',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
+        'NAME': 'dc0ckpt5hr14p3',
+        'USER': 'wjoqvvvmpsmhln',
+        'PASSWORD': '9eb0d95099e0981f514a9fa8b8f26a4f384778397f3ffbdee50b1592c813a6c1',
+        'HOST': 'ec2-54-86-224-85.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
