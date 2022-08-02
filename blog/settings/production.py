93d5ba88fa23882b57c8de417d4a05c2f1596158
SebastianHO <bastianhdezo@gmail.com>
2022-08-02 16:36:18 -0500

    env fix

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -15,7 +15,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dc0ckpt5hr14p3',
         'USER': 'wjoqvvvmpsmhln',
-        'PASSWORD': '    9eb0d95099e0981f514a9fa8b8f26a4f384778397f3ffbdee50b1592c813a6c1',
+        'PASSWORD': '9eb0d95099e0981f514a9fa8b8f26a4f384778397f3ffbdee50b1592c813a6c1',
         'HOST': 'ec2-54-86-224-85.compute-1.amazonaws.com',
         'PORT': '5432',
     }
